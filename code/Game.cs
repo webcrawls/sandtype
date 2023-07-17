f6cb55ee69b7f3ddb2c8286835b5615857abc865
webcrawls <github.com@kaden.sh>
2023-07-17 09:18:31 -0400

    ✨ initial functionality

--- code/Game.cs
+++ code/Game.cs
@@ -1,19 +1,58 @@
 ﻿using Sandbox;
+using System;
+using Sandbox.UI;
 using Sandtype.UI;
 
 namespace Sandtype;
 
 public class SandtypeGame : GameManager
 {
+	public String TypeTarget = "The quick brown fox jumps over the lazy dog.";
+
+	private String _content = "";
+
+	public bool IsComplete;
+	public String TypeContent
+	{
+		get => _content;
+		set
+		{
+			if ( _content == TypeTarget )
+			{
+				Complete();
+			}
+
+			_content = value;
+		}
+	}
+
 	public SandtypeGame()
 	{
 		if ( Game.IsServer ) return;
-		Game.RootPanel = new Main();
+		var panel = new Main();
+		panel.Game = this;
+		Game.RootPanel = panel;
 	}
+	
 	public override void ClientJoined( IClient client )
 	{
 		base.ClientJoined( client );
 	}
-	
+
+	public void ProcessInput()
+	{
+		
+	}
+
+	public void Reset()
+	{
+		TypeContent = "";
+	}
+
+	public void Complete()
+	{
+		IsComplete = true;
+		Log.Info( "You completed!" );
+	}
 
 }
